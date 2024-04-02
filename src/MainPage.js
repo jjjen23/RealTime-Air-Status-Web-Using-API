e7236f7854dd19bd566430381bf73c48834cafa7
jjjen23 <jelly2323@naver.com>
2024-04-02 16:42:18 +0900

    CAI그래프 수정

--- src/MainPage.js
+++ src/MainPage.js
@@ -11,49 +11,75 @@ import MapCom from "./components/MapCom";
 import SelectCom from "./components/SelectCom";
 
 
+
 function MainPage() {
   return(
     <div className={styles.desktop}>
-      <div className={styles.div}>
-
       {/* 전체 요소 : 크개 3개의 열 묶음으로 이동하게 구성 */}
-      <div style={{display:"flex", flexDirection:"row"}}>
+
+
+      {/* <div style={{ marginTop: '50px', marginLeft: '50px' }}>
+        <div style={{ width: '260px', height: '260px' }}>
+          <svg viewBox="0 0 260 260">
+            <circle cx="130" cy="130" r="120" fill="none" stroke="#515151" strokeWidth="16" />
+            <circle
+              cx="130"
+              cy="130"
+              r="120"
+              fill="none"
+              stroke="#ff655b"
+              strokeWidth="16"
+              // 75퍼센트로 계산된 비율임
+              strokeDasharray={2 * Math.PI * 130 * 0.75}
+            />
+          </svg>
+        </div>
+    </div> */}
+
+
+
+      <div className={styles.div}>
               {/* 왼쪽 한 묶음 : 네비바 */}
-              <div className={styles.navbar}>
-                  <div className={styles.logodiv}><img src={logoicon} alt="logoimg"/></div>
-                  <div className={styles.bottomgroup}>
-                    <div className={styles.favdiv}> <img src={favicon} alt="favicon"/></div>
-                    <div className={styles.homediv}><img src={homeicon} alt="homeicon"/></div>
-                  </div>
+              <div>
+                <div className={styles.navbar}>
+                    <div className={styles.logodiv}><img src={logoicon} alt="logoimg"/></div>
+                    <div className={styles.bottomgroup}>
+                      <div className={styles.favdiv}> <img src={favicon} alt="favicon"/></div>
+                      <div className={styles.homediv}><img src={homeicon} alt="homeicon"/></div>
+                    </div>
+                </div>
               </div>
 
               {/* 중앙 한 묶음 */}
-              <div style={{display:"flex", flexDirection:"column"}}>
-                <div className={styles.titlegroup}>
-                  <p className={styles.titlelocation}>서울 성동구</p>
-                  <div className={styles.titlefavimgdiv}><img className={styles.titlefavimg} src={favicon} alt="titlefavicon"/></div>
-                </div>
+              <div>
+                <div style={{display:"flex", flexDirection:"column", gap:"40px"}}>
+                    <div className={styles.titlegroup}>
+                      <p className={styles.titlelocation}>서울 성동구</p>
+                      <div className={styles.titlefavimgdiv}><img className={styles.titlefavimg} src={favicon} alt="titlefavicon"/></div>
+                    </div>
+  
+                  <div style={{display:"flex", flexDirection:"row", gap:"20px"}}>
+                    <div><SelectCom></SelectCom></div>
+                    <div><SelectCom></SelectCom></div>
+                  </div>
 
-                <div div style={{display:"flex", flexDirection:"row"}}>
-                  <div><SelectCom></SelectCom></div>
-                  <div style={{marginLeft:"63px"}}><SelectCom></SelectCom></div>
-                </div>
-                <div style={{marginLeft: "0px"}}><CAIComponent></CAIComponent></div>
-                <div style={{display:"flex", flexDirection:"row"}}>
-                      <div style={{marginTop:"0px", marginLeft:"0px"}}><FineDustcom></FineDustcom></div>
-                      <div style={{marginTop:"0px", marginLeft:"22px"}}><UltraDust></UltraDust></div>
+                  <div style={{display:"flex", marginLeft: "0px"}}><CAIComponent></CAIComponent></div>
+
+                  <div style={{display:"flex", flexDirection:"row", gap:"20px"}}>
+                        <div style={{marginTop:"0px", marginLeft:"0px"}}><FineDustcom></FineDustcom></div>
+                        <div style={{marginTop:"0px", marginLeft:"0px"}}><UltraDust></UltraDust></div>
+                  </div>
                 </div>
               </div>
 
               {/* 오른쪽 한 묶음 */}
-              <div style={{display:"flex", flexDirection:"column"}}>
-                <div className={styles.currentTime}><p>2024년 03월 25일 12:00 시 기준</p></div>
-                <div style={{marginTop:"0px", marginLeft:"0px"}}><NoticeCom></NoticeCom></div>
-                <div style={{marginTop:"0px", marginLeft:"0px"}}><MapCom></MapCom></div>
+              <div>
+                <div style={{display:"flex", flexDirection:"column" ,gap:"40px"}}>
+                  <div className={styles.currentTime}><p>2024년 03월 25일 12:00 시 기준</p></div>
+                  <div style={{marginTop:"0px", marginLeft:"0px"}}><NoticeCom></NoticeCom></div>
+                  <div style={{marginTop:"0px", marginLeft:"0px"}}><MapCom></MapCom></div>
+                </div>
               </div>
-      </div>
-      
-
       </div>
       
     </div>
--- src/components/CAIComponent.js
+++ src/components/CAIComponent.js
@@ -1,10 +1,12 @@
 import React from "react"
 import styles from "../style/caiComponent.module.css"
 import caigraphimg from "../img/caiGrapgImg.png"
+import { PieChart } from "react-minimal-pie-chart";
 
 function CAIComponent() {
   return(
     <div className={styles.totalgroup}>
+
       <div className={styles.boxtitle}>통합 대기환경 지수(CAI)</div>
 
       <div className={styles.caibox}>
@@ -25,13 +27,48 @@ function CAIComponent() {
         </div>
   
         {/* 수치 그래프 그룹 */}
-        <div className={styles.caiGraph}>
+        {/* <div className={styles.caiGraph}>
           <div className={styles.overlap}>
             <img className={styles.caigraphimg} src={caigraphimg} alt="caigraphimg"/>
             <div className={styles.value}>631</div>
             <div className={styles.state}>매우나쁨</div>
           </div>
-        </div>
+        </div> */}
+
+      {/* new !!! .. 수치 그래프 그룹 */}  
+      <div className={styles.graph}>
+        <PieChart
+            data = {[
+              {
+                value:631,
+                color : "#ff655b",
+                name : "name1",
+              },
+            ]}
+            reveal={60} 
+            lineWidth={13}
+            background="#f3f3f3"
+            lengthAngle={360}
+            rounded
+            animate
+            label={({dataEntry}) =>(
+              <React.Fragment>
+                <text x="50%" y="30%" textAnchor="middle" dominantBaseline="middle" fontSize="10px" fontFamily="SCDream5" fill="#ff655b">
+                  매우 나쁨
+                </text>
+                <text x="50%" y="58%" textAnchor="middle" dominantBaseline="middle" fontSize="30px" fontFamily="SUITE-ExtraBold" fill="#ff655b">
+                  {dataEntry.value}
+                </text>
+              </React.Fragment>
+            )}
+            // labelStyle={{
+            //   fontSize: "30px",
+            //   fill: "#ff655b",
+            //   fontFamily : "SUITE-ExtraBold",
+            // }}
+            // labelPosition={0}
+          />
+      </div>
 
       </div>
 
